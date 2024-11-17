8e18647fd886880f100847f23f82fc86d06e0bf9
Mayer24 <mpichun@utem.cl>
2024-11-17 20:29:14 -0300

    mejora 2 función

--- js/script.js
+++ js/script.js
@@ -2,14 +2,16 @@
 // la suma de las notas debe ser el retorno de una función y 
 // el promedio el retorno de otra función. Las notas son: 6,8,9,2,5,10.
 
+//suma
 const sumar =(a, b, c, d, e, f) => {
     return a + b + c + d + e + f
 }
 const sumatoria = sumar(6, 8, 9, 2, 5, 10)
 console.log('Resultado de la suma: ', sumatoria)
 
+//promedio
 const promedio = (a, b, c, d, e, f) => {
-    return (a + b + c + d + e + f) / 6
+    return (sumatoria) / 6
 }
-const resultado = promedio (6, 8, 9, 2, 5, 10)
+const resultado = promedio (sumatoria)
 console.log('Resultado del promedio: ', resultado)
